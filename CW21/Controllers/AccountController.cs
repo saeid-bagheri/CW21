b60ad0bdc7a891036a0643e7ab8b56247533eb04
saeid bagheri <saeidbagheri034@gmail.com>
2023-05-11 11:06:47 +0330

    Add project files.

---
+++ CW21/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using CW21.DAL.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CW21.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult RegisterPatient()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult RegisterPatient(Patient patient)
+        {
+            return View();
+        }
+    }
+}
